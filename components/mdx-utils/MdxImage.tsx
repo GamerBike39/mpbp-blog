f1876e001cd76fc17f8f436f56a16114486a9c84
GamerBike39 <champidub@gmail.com>
2023-07-05 15:44:33 +0200

    add animations photo pistes mdx

--- components/mdx-utils/MdxImage.tsx
+++ components/mdx-utils/MdxImage.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 interface MdxImageProps {
  src : string;
@@ -10,10 +11,24 @@ interface MdxImageProps {
 const MdxImage: FC<MdxImageProps> = ({ src, desc, className }) => {
   return (
     <>
-   <div className={`relative h-96 max-w-96 mx-auto md:h-[600px] md:w-[600px] my-5 ${className ? className : null}`}>
+   <motion.div className={`relative h-96 max-w-96 mx-auto md:h-[600px] md:w-[600px] my-5 ${className ? className : null}`}
+    initial={{
+    y: 200,
+    opacity: 0
+    }}
+   whileInView={{
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut"
+    }}
+   }
+
+   >
        <Image src={src} alt="image" fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 80vw" className='object-cover w-full h-full'/>
        <p className='my-10 pb-2 text-neutral-700'>{desc}</p>
-   </div>
+   </motion.div>
     </>
    );
 }
