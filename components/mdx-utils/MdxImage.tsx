2cafcac84ee7a48dedf8ed77ee31090291c198ff
GamerBike39 <champidub@gmail.com>
2023-07-05 13:34:54 +0200

    max-width for mdxIMage

--- components/mdx-utils/MdxImage.tsx
+++ components/mdx-utils/MdxImage.tsx
@@ -10,7 +10,7 @@ interface MdxImageProps {
 const MdxImage: FC<MdxImageProps> = ({ src, desc, className }) => {
   return (
     <>
-   <div className={`relative h-96 w-96 mx-auto md:h-[600px] md:w-[600px] my-5 ${className ? className : null}`}>
+   <div className={`relative h-96 max-w-96 mx-auto md:h-[600px] md:w-[600px] my-5 ${className ? className : null}`}>
        <Image src={src} alt="image" fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 80vw" className='object-cover w-full h-full'/>
        <p className='my-10 pb-2 text-neutral-700'>{desc}</p>
    </div>
