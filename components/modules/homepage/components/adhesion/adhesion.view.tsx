8cee985336da0ed50c7dad8a78a6352e7f7614e3
GamerBike39 <champidub@gmail.com>
2023-07-05 16:48:40 +0200

    remove divider adhesion

--- components/modules/homepage/components/adhesion/adhesion.view.tsx
+++ components/modules/homepage/components/adhesion/adhesion.view.tsx
@@ -14,7 +14,7 @@ const Adhesion = () => {
 
     return (
         <>
-        <div id='adhesion' className='py-10 divider-big-round-top relative'>
+        <div id='adhesion' className='py-10'>
         <div className="w-fit mx-auto">
                <Typography variant="h2" component="h2" theme="black" className="w-fit text-center py-12 z-10 beforeLine">
                   Association
